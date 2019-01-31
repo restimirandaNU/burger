bb7690b2b3cd0fcd049f7fd2483aadad9972a669
Resti Miranda <restimiranda@restis-mbp.attlocal.net>
2019-01-30 22:06:26 -0600

    Heroku deployment connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Wyble1948',
-    database: 'burgers_db'
-})
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    { else {
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'Wyble1948',
+            database: 'burgers_db'
+        });        
+    };
+    
 
 connection.connect(function(err){
     if(err)throw err;
