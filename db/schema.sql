7384cf2c0a0a202687efe911c3808963ab8bceb8
Resti Miranda <restimiranda@restis-mbp.attlocal.net>
2019-01-30 23:22:25 -0600

    adding schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT auto_increment NOT NULL,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
